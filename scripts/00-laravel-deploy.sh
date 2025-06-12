f385ad90cf74777d04f2c9698ec9fa6668cdee31
manjeetnethues <manjeet.kumar@123789.org>
2025-06-12 12:44:04 +0530

    made sqlite db writeable

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,7 +5,13 @@ echo "Running composer..."
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Creating SQLite DB if it doesn't exist..."
-[ -f database/database.sqlite ] || touch database/database.sqlite
+DB_PATH=database/database.sqlite
+if [ ! -f "$DB_PATH" ]; then
+  touch $DB_PATH
+  chmod 664 $DB_PATH
+fi
+
+chmod -R 775 storage bootstrap/cache
 
 echo "Caching config..."
 php artisan config:cache
